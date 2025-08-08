ecd845d078ceaa90e2e638ea8521bac29d539639
CesarJunco <76890299+CesarJunco@users.noreply.github.com>
2025-08-07 20:29:22 -0500

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://facturas.test/';
+$config['base_url'] = 'http://142.93.79.5/';
 
 /*
 |--------------------------------------------------------------------------
